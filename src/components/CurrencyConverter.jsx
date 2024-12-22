185bbec5bab9a03a80d8d29268d6260b707e6f8d
Nasr <nasr.asri10@gmail.com>
2024-12-22 11:55:42 +0100

    Add conversion logic

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -6,9 +6,10 @@ import { HiArrowsRightLeft } from "react-icons/hi2";
 const CurrencyConverter = () => {
   const [currencies, setCurrencies] = useState([]);
   const [amount, setAmount] = useState(1);
-
   const [fromCurrency, setFromCurrency] = useState("CHF");
   const [toCurrency, setToCurrency] = useState("EUR");
+  const [convertedAmount, setConvertedAmount] = useState(null);
+  const [converting, setConverting] = useState(false);
 
   // Currencies : https://api.frankfurter.dev/v1/currencies
   const fetchCurrencies = async () => {
@@ -28,8 +29,22 @@ const CurrencyConverter = () => {
 
   console.log(currencies);
 
-  const convertCurrency = () => {
-    // conversion logic
+  // Conversion : https://api.frankfurter.dev/v1/latest?base=CHF&symbols=EUR
+  const convertCurrency = async () => {
+    if (!amount) return;
+    setConverting(true);
+    try {
+      const res = await fetch(
+        `https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await res.json();
+
+      setConvertedAmount(data.rates[toCurrency] + " " + toCurrency);
+    } catch (error) {
+      console.error("Error fetching", error);
+    } finally {
+      setConverting(false);
+    }
   };
 
   const handleFavorite = (currency) => {
@@ -37,11 +52,10 @@ const CurrencyConverter = () => {
   };
 
   const swapCurrencies = () => {
-    setFromCurrency(toCurrency)
-    setToCurrency(fromCurrency)
-  }
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  };
 
-  // Conversion : https://api.frankfurter.dev/v1/latest?base=CHF&symbols=EUR
   return (
     <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md">
       <h2 className="mb-5 text-2xl font-semibold text-gray-700">
@@ -55,10 +69,13 @@ const CurrencyConverter = () => {
           currency={fromCurrency}
           setCurrency={setFromCurrency}
           handleFavorite={handleFavorite}
-          />
+        />
         {/* swap button */}
         <div className="flex justify-center -mb-5 sm:mb-0">
-          <button onClick={swapCurrencies} className="p-2 bg-gray-200 rounded-full cursor-pointer hover:bg-gray-300">
+          <button
+            onClick={swapCurrencies}
+            className="p-2 bg-gray-200 rounded-full cursor-pointer hover:bg-gray-300"
+          >
             <HiArrowsRightLeft className="text-xl text-gray-700" />
           </button>
         </div>
@@ -89,15 +106,19 @@ const CurrencyConverter = () => {
       <div className="flex justify-end mt-6">
         <button
           onClick={convertCurrency}
-          className="px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          className={`px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
+            ${converting ? "animate-pulse" : ""}
+            `}
         >
           Convert
         </button>
       </div>
 
-      <div className="mt-4 text-lg font-medium text-right text-green-600">
-        Converted Value: 1.47 EUR
-      </div>
+      {convertedAmount && (
+        <div className="mt-4 text-lg font-medium text-right text-green-600">
+          Converted Value: {convertedAmount}
+        </div>
+      )}
     </div>
   );
 };
